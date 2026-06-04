eefb1508c88c2e91fc5e15d7f159d600f21a0895
Kishore Pamarthi <kishorepamarthi001@gmail.com>
2026-06-05 00:58:10 +0530

    Add unit tests for App class functionality

---
+++ src/test/java/com/kishore/AppTest.java
@@ -0,0 +1,21 @@
+package com.kishore;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    
+    @Test
+    public void testAppMessage() {
+        App app = new App();
+        assertNotNull("App object create avvali", app);
+        System.out.println("Test 1 Pass: App object created");
+    }
+
+    @Test
+    public void testAddition() {
+        int result = 2 + 3;
+        assertEquals("2 + 3 = 5 avvali", 5, result);
+        System.out.println("Test 2 Pass: Addition correct");
+    }
+}
